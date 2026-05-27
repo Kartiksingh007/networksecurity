9747a4d30a162a9b897bef39bae7517ccaa53e84
Kartikey Rajput <kr2637804@gmail.com>
2026-05-27 16:41:15 +0530

    Data Transformation

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+from networksecurity.components.data_transformation import DataTransfromation
 import sys
 
 if __name__ == "__main__":
@@ -16,12 +17,22 @@ if __name__ == "__main__":
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data initiation Completed")
         print(dataingestionartifact)
+
         data_validation_config=DataValidationConfig(trainingpipelineconfig)
-        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        data_validation=DataValidation(data_ingestion_artifact=dataingestionartifact,data_validation_config=data_validation_config)
         logging.info("Initiate data Validation")
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("Data Validation Completed")
         print(data_validation_artifact)
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        logging.info("Data transformation started")
+
+        data_transformation=DataTransfromation(data_validation_artifact=data_validation_artifact,data_transformation_config=data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("Data Transformation completed")
+
+
 
         
 
